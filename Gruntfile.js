ffaa0ded6ba21bb69c7e151e53816e9663d4b1cd
felixyale <felix.yale@gmail.com>
2015-09-29 16:04:08 +0800

    graphicsmagick

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,29 +2,23 @@ module.exports = function(grunt) {
  
   // Project configuration. 
   grunt.initConfig({
-    css_sprite: {
-      options: {
-        'cssPath': '../images',
-        'processor': 'css',
-        'orientation': 'vertical',
-        'margin': 10
-      },
-      sprite: {
-        options: {
-          'style': 'dest/css/sprite.css',
-          'processor': 'css',
-          'margin': 10,
-          'retina': true
-        },
+    sprite:{
+      all: {
         src: ['src/images/*'],
-        dest: 'dest/images/sprite'
+        dest: 'dest/css/sprite.png',
+        destCss: 'dest/css/sprite.css',
+
+        retinaSrcFilter: ['src/images/*@2x.png'],
+        retinaDest: 'dest/css/sprite@2x.png'
       }
     }
   });
  
   // Load the plugin that provides the "css-sprite" task. 
-  grunt.loadNpmTasks('css-sprite');
- 
+  // grunt.loadNpmTasks('css-sprite');
+  grunt.loadNpmTasks('grunt-spritesmith');
+
   // Default task(s). 
-  grunt.registerTask('default', ['css_sprite']);
+  // grunt.registerTask('default', ['css_sprite']);
+  grunt.registerTask('default', ['sprite']);
 };
--- README.md
+++ README.md
@@ -4,6 +4,13 @@ csssprite example
 ## Getting Started
 If you haven't used [grunt][] before, be sure to check out the [Getting Started][] guide, as it explains how to create a [gruntfile][Getting Started] as well as install and use grunt plugins. Once you're familiar with that process, install this plugin with this command:
 
+### Install GM
+```shell
+~Mac
+https://github.com/aheckmann/gm
+brew install graphicsmagick
+```
+
 ```shell
 npm install
 ```
